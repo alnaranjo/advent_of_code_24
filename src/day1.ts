f0a05054554d8e69f97449b92f4796b44993376b
Alvaro Naranjo <alnp@me.com>
2024-12-02 00:16:31 -0500

    feat: day 1, part two

--- src/day1.ts
+++ src/day1.ts
@@ -52,33 +52,66 @@ const calculateDistances = (listA: number[], listB: number[]): number[] => {
     throw new Error('Unable to calculate distances. Mismatched list lenghts');
   }
 
+  // Sorted not in place so original data is preserved
+  const sortedListA = [...listA].sort((a, b) => a - b);
+  const sortedListB = [...listB].sort((a, b) => a - b);
+
   const distances: number[] = [];
-  for (let i = 0; i < listA.length; ++i) {
-    const distance = Math.abs(listA[i] - listB[i]);
+  for (let i = 0; i < sortedListA.length; ++i) {
+    const distance = Math.abs(sortedListA[i] - sortedListB[i]);
     distances.push(distance);
   }
 
   return distances;
 };
 
-const calculateTotalDistance = (distances: number[]): number => {
-  return distances.reduce((prev, curr) => {
+const calculateSimilarityScores = (
+  listA: number[],
+  listB: number[]
+): number[] => {
+  if (listA.length !== listB.length) {
+    throw new Error(
+      'Unable to calculate similarity score. Mismatched list lenghts'
+    );
+  }
+
+  const appereances: Map<number, number> = new Map();
+  for (let i = 0; i < listB.length; ++i) {
+    const target = listB[i];
+    const count = appereances.get(target);
+    appereances.set(target, (count || 0) + 1);
+  }
+
+  const scores: number[] = [];
+  listA.forEach((value) => {
+    const count = appereances.get(value) || 0;
+    const score = value * count;
+    scores.push(score);
+  });
+
+  return scores;
+};
+
+const calculateTotal = (list: number[]): number => {
+  return list.reduce((prev, curr) => {
     return prev + curr;
   }, 0);
 };
 
 const main = () => {
-  const filename = 'day1/part1.txt';
+  const filename = 'day1/data.txt';
   const input = readInputData(filename);
   const data = parseInputData(input);
 
-  data.listA.sort((a, b) => a - b);
-  data.listB.sort((a, b) => a - b);
-
+  // Part One
   const distances = calculateDistances(data.listA, data.listB);
-  const totalDistance = calculateTotalDistance(distances);
-
+  const totalDistance = calculateTotal(distances);
   console.log({ totalDistance });
+
+  // Part Two
+  const scores = calculateSimilarityScores(data.listA, data.listB);
+  const totalScore = calculateTotal(scores);
+  console.log({ totalScore });
 };
 
 main();
