140eb192c1ab9d63cc5c42557b939df0c305eb9c
Alvaro Naranjo <alnp@me.com>
2024-12-03 17:44:54 -0500

    feat: day 2, part two

--- src/day2.ts
+++ src/day2.ts
@@ -38,19 +38,18 @@ const parseInputData = (input: string): Data => {
   return { reports };
 };
 
-type Safeness = 'safe' | 'unsafe';
-const calculateReportSafeness = (levels: number[]): Safeness => {
+const isReportSafe = (levels: number[]): boolean => {
   let totalLevels = levels.length;
   let totalIncreases = 0;
   let totalDecreases = 0;
 
   for (let i = 0; i < totalLevels - 1; ++i) {
-    const a = levels[i];
-    const b = levels[i + 1];
-    const difference = a - b;
+    const previous = levels[i];
+    const current = levels[i + 1];
+    const difference = current - previous;
 
     if (Math.abs(difference) < 1 || Math.abs(difference) > 3) {
-      return 'unsafe';
+      return false;
     }
 
     if (difference < 0) {
@@ -64,24 +63,68 @@ const calculateReportSafeness = (levels: number[]): Safeness => {
     totalIncreases === totalLevels - 1 ||
     totalDecreases === totalLevels - 1
   ) {
-    return 'safe';
+    return true;
   }
 
-  return 'unsafe';
+  return false;
 };
 
-const calculateReportsSafeness = (
+const getPermutations = (list: number[]): number[][] => {
+  const permutations: number[][] = [];
+
+  for (let i = 0; i < list.length; ++i) {
+    const modified = [...list];
+    modified.splice(i, 1);
+    permutations.push(modified);
+  }
+
+  return permutations;
+};
+
+const calculatePartOne = (
   reports: Report[]
 ): { totalSafe: number; totalUnsafe: number } => {
   let totalSafe = 0;
   let totalUnsafe = 0;
 
-  const list = reports.map((report) => calculateReportSafeness(report.levels));
-  list.forEach((item) => {
-    if (item === 'safe') {
+  reports.forEach((report) => {
+    const isSafe = isReportSafe(report.levels);
+    if (isSafe) {
       totalSafe += 1;
     } else {
       totalUnsafe += 1;
+    }
+  });
+
+  return { totalSafe, totalUnsafe };
+};
+
+const calculatePartTwo = (
+  reports: Report[]
+): { totalSafe: number; totalUnsafe: number } => {
+  let totalSafe = 0;
+  let totalUnsafe = 0;
+
+  reports.forEach((report) => {
+    const isSafe = isReportSafe(report.levels);
+
+    if (isSafe) {
+      totalSafe += 1;
+    } else {
+      const permutations = getPermutations(report.levels);
+      let safeFound = false;
+      for (let i = 0; i < permutations.length; ++i) {
+        safeFound = isReportSafe(permutations[i]);
+        if (safeFound) {
+          break;
+        }
+      }
+
+      if (safeFound) {
+        totalSafe += 1;
+      } else {
+        totalUnsafe += 1;
+      }
     }
   });
 
@@ -93,9 +136,11 @@ const main = () => {
   const input = readFileContents(filename);
   const data = parseInputData(input);
 
-  const totalReports = calculateReportsSafeness(data.reports);
+  const partOne = calculatePartOne(data.reports);
+  console.log({ partOne });
 
-  console.log({ totalReports });
+  const partTwo = calculatePartTwo(data.reports);
+  console.log({ partTwo });
 };
 
 main();
