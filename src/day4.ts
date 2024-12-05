216019f1f9bcecc35ec77f2044f2ea7e175b0394
Alvaro Naranjo <alnp@me.com>
2024-12-04 20:52:06 -0500

    feat: day 4, part two

--- src/day4.ts
+++ src/day4.ts
@@ -1,59 +1,83 @@
 import { readFileContents } from './utils/file';
 
+type Coordinates = {
+  x: number;
+  y: number;
+};
+
 type Data = {
-  x: Map<string, boolean>;
-  m: Map<string, boolean>;
-  a: Map<string, boolean>;
-  s: Map<string, boolean>;
+  xCoordinates: Map<string, boolean>;
+  mCoordinates: Map<string, boolean>;
+  aCoordinates: Map<string, boolean>;
+  sCoordinates: Map<string, boolean>;
+  width: number;
+  height: number;
+};
+
+const serializeKey = (coordinates: Coordinates): string =>
+  `${coordinates.x},${coordinates.y}`;
+
+const deserializeKey = (key: string): Coordinates => {
+  const values = key.split(',');
+  return {
+    x: parseInt(values[0], 10),
+    y: parseInt(values[1], 10),
+  };
 };
 
 const parseFileContents = (fileContents: string): Data => {
   const lines = fileContents.split('\n');
 
-  const xSeen: Map<string, boolean> = new Map();
-  const mSeen: Map<string, boolean> = new Map();
-  const aSeen: Map<string, boolean> = new Map();
-  const sSeen: Map<string, boolean> = new Map();
+  const xCoordinates: Map<string, boolean> = new Map();
+  const mCoordinates: Map<string, boolean> = new Map();
+  const aCoordinates: Map<string, boolean> = new Map();
+  const sCoordinates: Map<string, boolean> = new Map();
 
-  for (let y = 0; y < lines.length; ++y) {
+  const width = lines[0].length;
+  const height = lines.length;
+
+  for (let y = 0; y < height; ++y) {
     const line = lines[y];
-    for (let x = 0; x < line.length; ++x) {
+    for (let x = 0; x < width; ++x) {
       const letter = line[x];
-      const coordinates = `${x},${y}`;
+      const key = serializeKey({ x, y });
+
       if (letter === 'X') {
-        xSeen.set(coordinates, true);
+        xCoordinates.set(key, true);
         continue;
       }
 
       if (letter === 'M') {
-        mSeen.set(coordinates, true);
+        mCoordinates.set(key, true);
         continue;
       }
 
       if (letter === 'A') {
-        aSeen.set(coordinates, true);
+        aCoordinates.set(key, true);
         continue;
       }
 
       if (letter === 'S') {
-        sSeen.set(coordinates, true);
+        sCoordinates.set(key, true);
         continue;
       }
     }
   }
 
-  return { x: xSeen, m: mSeen, a: aSeen, s: sSeen };
-};
-
-const getCoordinates = (value: string): { x: number; y: number } => {
-  const [x, y] = value.split(',');
-  return { x: parseInt(x), y: parseInt(y) };
+  return {
+    xCoordinates,
+    mCoordinates,
+    aCoordinates,
+    sCoordinates,
+    width,
+    height,
+  };
 };
 
 const getDirections = (
-  target: string,
+  target: Coordinates,
   list: Map<string, boolean>
-): { x: number; y: number }[] => {
+): Coordinates[] => {
   const topLeft = { x: -1, y: -1 };
   const top = { x: 0, y: -1 };
   const topRight = { x: 1, y: -1 };
@@ -74,12 +98,15 @@ const getDirections = (
     bottomRight,
   ];
 
-  const results: { x: number; y: number }[] = [];
+  const results: Coordinates[] = [];
 
   for (const direction of directions) {
-    const position = getCoordinates(target);
-    const location = `${position.x + direction.x},${position.y + direction.y}`;
-    const found = list.get(location);
+    const coordinates = {
+      x: target.x + direction.x,
+      y: target.y + direction.y,
+    };
+    const key = serializeKey(coordinates);
+    const found = list.get(key);
     if (found) {
       results.push(direction);
     }
@@ -88,32 +115,99 @@ const getDirections = (
   return results;
 };
 
-const solvePartOne = (data: Data): number => {
-  console.log(data);
+const hasData = (target: Coordinates, list: Map<string, boolean>): boolean => {
+  const key = serializeKey(target);
+  return list.has(key);
+};
 
+const getValue = (target: Coordinates, data: Data): string => {
+  if (hasData(target, data.xCoordinates)) {
+    return 'X';
+  } else if (hasData(target, data.mCoordinates)) {
+    return 'M';
+  } else if (hasData(target, data.aCoordinates)) {
+    return 'A';
+  } else {
+    return 'S';
+  }
+};
+
+const solvePartOne = (data: Data): number => {
   let total = 0;
 
-  const keys = Array.from(data.x.keys());
-  for (let i = 0; i < keys.length; ++i) {
-    const xLocation = keys[i];
-    let position = getCoordinates(xLocation);
-    const directions = getDirections(xLocation, data.m);
+  const xCoordinates = Array.from(data.xCoordinates.keys());
+  for (let i = 0; i < xCoordinates.length; ++i) {
+    const coordinates = deserializeKey(xCoordinates[i]);
+    const directions = getDirections(coordinates, data.mCoordinates);
 
     for (let j = 0; j < directions.length; ++j) {
       const direction = directions[j];
-      console.log({ xLocation, direction });
-
-      const aFound = data.a.has(
-        `${position.x + direction.x * 2},${position.y + direction.y * 2}`
+      const aFound = hasData(
+        {
+          x: coordinates.x + direction.x * 2,
+          y: coordinates.y + direction.y * 2,
+        },
+        data.aCoordinates
       );
-      const sFound = data.s.has(
-        `${position.x + direction.x * 3},${position.y + direction.y * 3}`
+      const sFound = hasData(
+        {
+          x: coordinates.x + direction.x * 3,
+          y: coordinates.y + direction.y * 3,
+        },
+        data.sCoordinates
       );
 
       if (aFound && sFound) {
         total += 1;
       }
     }
+  }
+
+  return total;
+};
+
+const solvePartTwo = (data: Data): number => {
+  let total = 0;
+
+  const topLeft = { x: -1, y: -1 };
+  const bottomLeft = { x: -1, y: 1 };
+  const bottomRight = { x: 1, y: 1 };
+  const topRight = { x: 1, y: -1 };
+
+  const directions = [topLeft, bottomLeft, bottomRight, topRight];
+
+  const aCoordinates = Array.from(data.aCoordinates.keys());
+  for (let i = 0; i < aCoordinates.length; ++i) {
+    const coordinates = deserializeKey(aCoordinates[i]);
+
+    // skip borders
+    if (
+      coordinates.x === 0 ||
+      coordinates.x === data.width - 1 ||
+      coordinates.y === 0 ||
+      coordinates.y === data.height - 1
+    ) {
+      continue;
+    }
+
+    const corners = directions.map((direction) => {
+      const target = {
+        x: coordinates.x + direction.x,
+        y: coordinates.y + direction.y,
+      };
+      return getValue(target, data);
+    });
+
+    if (
+      corners.includes('X') ||
+      corners.includes('A') ||
+      corners[0] === corners[2] ||
+      corners[1] === corners[3]
+    ) {
+      continue;
+    }
+
+    total += 1;
   }
 
   return total;
@@ -127,8 +221,8 @@ const main = () => {
   const partOne = solvePartOne(parsedData);
   console.log({ partOne });
 
-  // const partTwo = solvePartOne(parsedData);
-  // console.log({partTwo});
+  const partTwo = solvePartTwo(parsedData);
+  console.log({ partTwo });
 };
 
 main();
