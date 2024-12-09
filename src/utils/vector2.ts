4c090b6f2725a97c95b0298b6ae1576eff3994cf
Alvaro Naranjo <alnp@me.com>
2024-12-09 18:43:29 -0500

    feat: vector2

---
+++ src/utils/vector2.ts
@@ -0,0 +1,26 @@
+export type Vector2 = {
+  x: number;
+  y: number;
+};
+
+export const isVector2InBounds = (
+  v: Vector2,
+  width: number,
+  height: number
+): boolean => {
+  return v.x >= 0 && v.x < width && v.y >= 0 && v.y < height;
+};
+
+export const vector2ToString = (v: Vector2) => `${v.x},${v.y}`;
+
+export const vector2FromString = (v: string): Vector2 => {
+  const values = v.split(',');
+  if (values.length !== 2) {
+    throw new Error('Unable to parse vector2. Invalid number of elements');
+  }
+  try {
+    return { x: parseInt(values[0], 10), y: parseInt(values[1], 10) };
+  } catch (error) {
+    throw new Error('Unable to parse vetor2');
+  }
+};
